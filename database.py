565f3010fe37ea088938dd050a0378037f327610
Ananya Joshi <73184612+AJgthb2002@users.noreply.github.com>
2021-03-04 13:22:30 +0530

    implemented sign up option

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
-from allotment_mechanism import * 
-from csv import * 
+from allotment_mechanism import *
+import json
+from csv import *
 
 mymachine= Allotment_machanism()
 user= None
-username= None
+user_name= None
+firstname=None
 
 class Data:
       
@@ -25,60 +27,59 @@ class Data:
     |  4. Electronics    |         120    |
     ---------------------------------------
     """
-        self.edit_menu= """
-                WHAT DO YOU WANT TO EDIT?
-    ---------------------------------------
-    |     Option          |   Enter choice |
-    ---------------------------------------      
-    |  1. E-mail          |          0     |
-    |  2. Preference1     |          1     |
-    |  3. Preference 2    |          2     |
-    |  4. Preference  3   |          3     |
-    ---------------------------------------
-    """
 
-    def set_userinfo(self, usr, usrnm):
-        global user, username
+
+    def set_userinfo(self, usr, usrnm, fname):
+        global user, user_name, firstname
         user = usr
-        username = usrnm
+        user_name = usrnm
+        firstname = fname
+        
 
 
     def view_seatmatrix(self):
         print(self.seat_matrix)
-        
-        
 
     def find_record(self, name):
-        with open("datasheet.csv",'r') as f:
-            reader_object = reader(f)
-            for row in reader_object:
-                if(row[0]==name):
-                    return 1
-        return 0 
-    
+        # return 1 if record of name found, 0 otherwise.
+        # if record found, print it here itself.
+        with open('DataSheet.csv', 'r') as datasheet:
+            csv_reader = reader(datasheet)
+            for row in csv_reader:
+                try:
+                    if row[0]== name:
+                        print("Applicant details: ")
+                        for i in range(0,7):
+                            print(row[i], end="   ")
+                        return 1
+                except IndexError:
+                    return 0            
+        return 0
+
+    def append_to_csv(self, record):
+        with open("DataSheet.csv", 'a+', newline="") as datasheet:
+            csv_writer = writer(datasheet)
+            csv_writer.writerow(record)
+
     def register(self):
         # search user name, if not found, create a new student record
-        name = input("Enter your name: ")
-        if self.find_record(name)==0:
-            
-            surname = input("Enter your surname: ")
-            email = input("Enter your email: ")
-            marks = int(input("Enter your marks: "))
-            pref1 = int(input("Enter your preference 1: "))
-            pref2 = int(input("Enter your preference 2: "))
-            pref3 = int(input("Enter your preference 3: "))
-            allotment = "--"
-            record = [name, surname, email, marks, pref1, pref2, pref3, allotment]
-        
-            with open('datasheet.csv', 'a+', newline='') as f_object: 
-    
-                writer_object = writer(f_object) 
-                writer_object.writerow(record) 
-                f_object.close()
-            
-        else:
-            print("You have registered successfully.")
-            # print the student record    
+        with open("signup_data.json", 'r') as signupdata:
+            data= json.load(signupdata)
+            if self.find_record(firstname)==0:
+                print("Applicant details:\n")
+                print("Name= " + data[user_name]["name"] +" "+data[user_name]["surname"])
+                print("Email= "+data[user_name]["email"])
+                marks= int(input("Enter your marks: "))
+                print("\nPreference codes: (Computer:0, IT:1, Mechanical:2, Electronics:3)\n")
+                pref1= int(input("Enter Preference 1:"))
+                pref2= int(input("Enter Preference 2:"))
+                pref3= int(input("Enter Preference 3:"))
+                stud_rec=[data[user_name]["name"], data[user_name]["surname"], marks, data[user_name]["email"],pref1, pref2, pref3,"-"]
+                self.append_to_csv(stud_rec)
+                print("\nRegistration Successful!")
+            else:
+                print("\nYou have registered successfully.")
+                
 
     def view_all_registrations(self):
         # print table of all records (name, surname, email, marks)
@@ -103,18 +104,12 @@ class Data:
     def search_student(self):
         if user==1:
             # for user= student, show only his record.(all columns)
-            name = input("Enter your name: ")
-            with open("datasheet.csv",'r') as f:
-                reader_object = reader(f)
-                for row in reader_object:
-                    if(row[0]==name):
-                    #print(f"This is the application status of {username}")
-                         print(row)
-            
+            print(f"This is the application status of {user_name}")
+            pass
         if user==2:
             # for user= admin, show details of any student
             print("input the name of a student to search their details")
-            
+            pass
 
     def get_cutoff_marks(self):
         # cutoff marks= in the rankwise sorted table, find last row of each branch. Corresponding marks of that row is cutoff marks.
@@ -137,35 +132,51 @@ class Data:
                
 
     def edit_record(self):
-        #allow only if allotment is not yet done
-        if self.find_record(username):
+    
+        if self.find_record(firstname):
             if mymachine.allotment_done== True:
-                print("Cannot Edit record now. Your allotment result is: ")
-                # print allotment result
+                with open('DataSheet.csv', 'r') as datasheet:
+                    csv_reader = reader(datasheet)
+                    for row in csv_reader:
+                        if row[0]== firstname:
+                            print("Cannot Edit record now. Your allotment result is: "+ row[7])
+                
             else:
-                #print("edit your record here") 
-                name = input("Enter your name: ")
-                with open("datasheet.csv",'r') as f:
-                    reader_object = reader(f)
-                    for row in reader_object:
-                        if(row[0]==name):
-                            to_edit= int(input(edit_menu))s
-                            row[to_edit+2]= input("Enter new value:")
-                            
-                            
-                print()
+                pass
+                # with open('DataSheet.csv', 'r+') as datasheet:
+                #     csv_writer = writer(datasheet)
+                #     csv_reader= reader(datasheet)
+                #     for row in csv_reader:
+                #         if row[0]== firstname:
+                #             print("\nName= "+ row[0] +" " + row[1])
+                #             print("Marks= "+ row[2])
+                #             row[3]= input("Enter your email id: ")
+                #             row[4]=int(input("Enter preference 1: "))
+                #             row[5]=int(input("Enter preference 2: "))
+                #             row[6]=int(input("Enter preference 3: "))
+                #             print("\nRecord updated successfully!")
+
         else:
             print("Student Record not found. Please register yourself.")           
 
     def delete_record(self):
         #allow only if allotment is not yet done
-        if self.find_record():
+        if self.find_record(firstname):
             if mymachine.allotment_done== True:
-                print("Cannot withdraw the application now. Your allotment result is: ")
+                print("Cannot withdraw the application now. Allotment Process is done.")
             else:
                 confirmation = (input("Do you wish to remove your record permanently? (press 'y'/'n') "))
                 if confirmation == 'y' :
-                    print("Your application was removed from list")    
+                    pass
+                    # with open("DataSheet.csv",'a+') as datasheet:
+                    #     csv_reader= reader(datasheet)
+                    #     for row in csv_reader:
+                    #         if row[0] != firstname:
+                    #             writer.writerow(row) ; # write all non-matching rows
+                    #         else:
+                    #             pass
+        
+                    #     print("Your application was removed.")    
                 else:
                     return    
         else:
@@ -192,6 +203,6 @@ class Data:
 
     student_options=[view_seatmatrix, register, search_student, edit_record, delete_record, view_cutoff_marks]
     admin_options=[mymachine.run_allotment, view_all_registrations, view_allotment_result, view_branchwise_allotment, search_student, students_without_allotment, vacancies_left]   
-    
+
 
     
--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 from os import system, name
 from database import *
 import time
+import json
 
 mydata= Data()
 
@@ -14,7 +15,7 @@ class Menu:
   def __init__(self):
     
     self.user= None
-    self.username= None
+    # self.username= None
 
     self.student_menu = """
                                                          MENU
@@ -46,9 +47,10 @@ class Menu:
     self.login_menu= """
                                                       MENU
                                             --------------------------        
-                                            |  1. Login as Student   |
-                                            |  2. Login as Admin     |
-                                            |  3. Exit portal        |
+                                            |  1. Sign Up            |
+                                            |  2. Login as Student   |
+                                            |  3. Login as Admin     |
+                                            |  4. Exit portal        |
                                             --------------------------
       """
 
@@ -57,14 +59,82 @@ class Menu:
     print(self.login_menu)
     user_inp = int(input("Enter your choice: "))
     
-    while (user_inp > 3 or user_inp < 1):
+    while (user_inp > 4 or user_inp < 1):
         user_inp = int(input("Invalid. Enter your choice again: "))
     self.user = user_inp
-    if self.user!=3:
-        self.username = input("Enter user name: ")
-        mydata.set_userinfo(self.user, self.username)
-        print(f"\n Welcome, {self.username.capitalize()}!")
+
+    if self.user==1:    # signup
+        self.signup()
+
+    if self.user==2:    #login as student
+        username = input("Enter your username: ")
+        pwd = input("Enter password: ")
+        # if username and pwd match, set user=2 and user's name= find from file.
+        with open("signup_data.json",'r') as datafile:
+            try:
+                data= json.load(datafile)
+            except json.JSONDecodeError:
+                print(f"Error: Username {username} not found! Please Sign up on the portal.") 
+                time.sleep(2)
+                self.login()     
+            else:
+                if username in data:
+                    if pwd == data[username]["password"]:
+                        mydata.set_userinfo(self.user, username, data[username]["name"])
+                        print(f"\n Welcome, {username.capitalize()}!")
+                        time.sleep(2)
+                    else:
+                        print("\nError: Incorrect Password. Please try again.")
+                        time.sleep(2)
+                        self.login()
+                else:
+                    print(f"\nError: Username {username} not found! Please Sign up on the portal.")
+                    time.sleep(2) 
+                    self.login()   
+
+
+    if self.user==3:      # login as admin
+        pwd = input("Enter password: ") 
+        # admin password = 12345
+        if pwd == "12345":
+            print("\n Welcome, Admin!")
+            time.sleep(2)
+        else:
+            print("\nError: Incorrect Password. Please try again.") 
+            time.sleep(2)
+            self.login()                 
+
+
+  def signup(self):
+    clear()
+    name= input("Enter your name: ")
+    surname= input("Enter your surname: ")
+    email= input("Enter your email id: ")
+    username= input("Set a username: ")
+    pwd= input("Set a password: ")
+    new_data_dict={
+        username:{
+            "name": name,
+            "surname" : surname,
+            "email": email,
+            "password": pwd,
+        }
+    }
+    try:
+        with open("signup_data.json",'r') as datafile:
+            data= json.load(datafile)
+            
+    except :
+        with open("signup_data.json",'w') as datafile:
+            json.dump(new_data_dict, datafile, indent=4)
+    else:
+        data.update(new_data_dict)
+        with open("signup_data.json",'w') as datafile:
+            json.dump(data, datafile, indent=4)
+    finally:
+        print("Sign Up successful!")
         time.sleep(2)
+
         
   def menu_for_student(self):
     clear() 
@@ -81,7 +151,7 @@ class Menu:
             mydata.student_options[choice-1](mydata)    
       
     return
-    # self.login()
+    
 
   def menu_for_admin(self):
     clear() 
